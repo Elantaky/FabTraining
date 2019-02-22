efcf77a4d0c01a9e4dddcc8f570fcd431a472b58
Elantaky <38358198+Elantaky@users.noreply.github.com>
2019-02-23 00:27:18 +0200

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/zecode/z/fabtraining/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/zecode/z/fabtraining/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
+//saleh 
         assertEquals("com.zecode.z.fabtraining", appContext.getPackageName());
     }
 }
